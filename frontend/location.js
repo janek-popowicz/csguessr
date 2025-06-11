34c2fb2b42b0ca95cdce877ef54652c62973f6df
Janek Popowicz <jan.j.popowicz@gmail.com>
2025-06-11 19:53:51 +0200

    removed hard-code from get image api service

--- frontend/location.js
+++ frontend/location.js
@@ -1,7 +1,7 @@
 function loadMainImage() {
     const mainView = document.getElementById('main-view');
     const img = document.createElement('img');
-    img.src = '/test.png';
+    img.src = '/26-April-21-43-06-05.png';
     img.alt = 'Game location';
     mainView.appendChild(img);
 }
--- server.js
+++ server.js
@@ -10,14 +10,27 @@ app.get('/map.osm', (req, res) => {
   res.sendFile(path.join(__dirname, 'resources', 'Urblin', 'urblin.osm'));
 });
 
-// Dodaj endpoint dla obrazów testowych
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, 'resources', 'Urblin', 'test.png'));
+// Endpoint dla dowolnego obrazu
+app.get('/:filename', (req, res) => {
+  const filename = req.params.filename;
+  // Sprawdź czy to na pewno plik obrazu
+  if (filename.match(/\.(png|jpg|jpeg|gif)$/i)) {
+    res.sendFile(path.join(__dirname, 'resources', 'Urblin', filename));
+  } else {
+    res.status(400).send('Dozwolone tylko pliki obrazów');
+  }
 });
-
-// Można też dodać ogólny endpoint dla wszystkich obrazów
-app.use('/images', express.static(path.join(__dirname, 'resources', 'Urblin')));
 
 app.listen(3000, () => {
   console.log('Serwer działa na http://localhost:3000');
+});
+
+app.get('/get_game', (req, res) => {
+  const gameData = {
+    name: 'Urblin',
+    description: 'Gra planszowa osadzona w fikcyjnym mieście Urblin.',
+    image: '/26-April-21-43-06-05.png',
+    map: '/map.osm'
+  };
+  res.json(gameData);
 });
